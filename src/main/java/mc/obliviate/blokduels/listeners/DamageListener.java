b7215733baedadbe694f3aa9eb8c82add65581f8
Obliviated <hamzacoskun41@gmail.com>
2021-11-28 16:12:18 +0300

    Fixed spectators can hit non-player entities.

--- src/main/java/mc/obliviate/blokduels/listeners/DamageListener.java
+++ src/main/java/mc/obliviate/blokduels/listeners/DamageListener.java
@@ -7,6 +7,7 @@ import mc.obliviate.blokduels.team.Member;
 import mc.obliviate.blokduels.utils.MessageUtils;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -24,39 +25,44 @@ public class DamageListener implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onPreDeath(EntityDamageEvent e) {
-		if (e.getEntity() instanceof Player) {
-			final Player victim = (Player) e.getEntity();
-
-			final Member member = DataHandler.getMember(victim.getUniqueId());
-			if (member == null) return;
-			if (member.getTeam() == null) Bukkit.broadcastMessage("team is null");
-			if (member.getTeam().getGame() == null) Bukkit.broadcastMessage("game is null");
-
-
+		if (!e.getEntityType().equals(EntityType.PLAYER)) {
 			if (e instanceof EntityDamageByEntityEvent && ((EntityDamageByEntityEvent) e).getDamager() instanceof Player) {
-				final Member attackerMember = DataHandler.getMember(((EntityDamageByEntityEvent) e).getDamager().getUniqueId());
-				if (attackerMember == null) {
-					e.setCancelled(true);
-					return;
-				}
-				if (attackerMember.getTeam().equals(member.getTeam())) {
-					e.setCancelled(true);
-					attackerMember.getPlayer().sendMessage("Takımındaki adama vuramazsın ke.");
-					return;
-				}
-				//todo spectators can hit non-player entities.
-				final SpectatorData spectatorData = member.getTeam().getGame().getSpectatorData();
-				if (spectatorData.isSpectator(victim) || spectatorData.isSpectator(attackerMember.getPlayer())) {
-					e.setCancelled(true);
-					member.getPlayer().sendMessage("Izleyiciyken birine vuramazsın veya izleyicilere vuramazsın ke.");
-				}
-			}
-
-			if (e.getFinalDamage() >= victim.getHealth()) {
+				final Member member = DataHandler.getMember((((EntityDamageByEntityEvent) e).getDamager()).getUniqueId());
+				if (member == null) return;
 				e.setCancelled(true);
-				member.getTeam().getGame().onDeath(member);
-
 			}
+			return;
+		}
+
+		final Player victim = (Player) e.getEntity();
+
+		final Member member = DataHandler.getMember(victim.getUniqueId());
+		if (member == null) return;
+		if (member.getTeam() == null) Bukkit.broadcastMessage("team is null");
+		if (member.getTeam().getGame() == null) Bukkit.broadcastMessage("game is null");
+
+
+		if (e instanceof EntityDamageByEntityEvent && ((EntityDamageByEntityEvent) e).getDamager() instanceof Player) {
+			final Member attackerMember = DataHandler.getMember(((EntityDamageByEntityEvent) e).getDamager().getUniqueId());
+			if (attackerMember == null) {
+				e.setCancelled(true);
+				return;
+			}
+			if (attackerMember.getTeam().equals(member.getTeam())) {
+				e.setCancelled(true);
+				//friendly protect
+				return;
+			}
+			final SpectatorData spectatorData = member.getTeam().getGame().getSpectatorData();
+			if (spectatorData.isSpectator(victim) || spectatorData.isSpectator(attackerMember.getPlayer())) {
+				e.setCancelled(true);
+				//spectator protect
+			}
+		}
+
+		if (e.getFinalDamage() >= victim.getHealth()) {
+			e.setCancelled(true);
+			member.getTeam().getGame().onDeath(member);
 		}
 	}
 
