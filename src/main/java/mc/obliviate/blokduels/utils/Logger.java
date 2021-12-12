d685d51afbb4af27ebf188c945ca77a8a7991df3
Obliviated <hamzacoskun41@gmail.com>
2021-12-12 06:45:36 +0300

    Added log string length limit.

--- src/main/java/mc/obliviate/blokduels/utils/Logger.java
+++ src/main/java/mc/obliviate/blokduels/utils/Logger.java
@@ -8,15 +8,22 @@ public class Logger {
 
 	public static void debug(String message) {
 		if (!debugModeEnabled) return;
-		Bukkit.getConsoleSender().sendMessage("[DEBUG] " + message);
+		Bukkit.getConsoleSender().sendMessage("[DEBUG] " + fixLength(message));
 	}
 
 	public static void severe(String message) {
-		Bukkit.getLogger().severe(message);
+		Bukkit.getLogger().severe(fixLength(message));
 	}
 
 	public static void error(String message) {
-		Bukkit.getConsoleSender().sendMessage("[ERROR INFO] " + message);
+		Bukkit.getConsoleSender().sendMessage("[ERROR INFO] " + fixLength(message));
+	}
+
+	private static String fixLength(String string) {
+		if (string.length() > 500) {
+			return string.substring(0, 500) + "...";
+		}
+		return string;
 	}
 
 }
