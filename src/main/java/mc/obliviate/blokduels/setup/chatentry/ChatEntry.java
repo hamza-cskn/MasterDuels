d4a83497113541477650beaff2bc48d0128e262e
Obliviated <hamzacoskun41@gmail.com>
2022-01-01 17:01:24 +0300

    Fixed async chat bug.

--- src/main/java/mc/obliviate/blokduels/setup/chatentry/ChatEntry.java
+++ src/main/java/mc/obliviate/blokduels/setup/chatentry/ChatEntry.java
@@ -1,7 +1,9 @@
 package mc.obliviate.blokduels.setup.chatentry;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.Plugin;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,11 +12,24 @@ import java.util.UUID;
 public class ChatEntry {
 
 	private static final Map<UUID, ChatEntry> entryMap = new HashMap<>();
-
+	private final Plugin plugin;
 	public Entry entry;
 
-	public ChatEntry(UUID uuid) {
+	public ChatEntry(UUID uuid, Plugin plugin) {
+		this.plugin = plugin;
 		entryMap.put(uuid, this);
+	}
+
+	public static void trigger(AsyncPlayerChatEvent e) {
+		final Player sender = e.getPlayer();
+		final ChatEntry chatEntry = entryMap.get(sender.getUniqueId());
+		if (chatEntry == null || chatEntry.getEntry() == null) return;
+		e.setCancelled(true);
+		Bukkit.getScheduler().runTask(chatEntry.plugin, () -> {
+			chatEntry.getEntry().entry(e);
+		});
+		entryMap.remove(sender.getUniqueId());
+
 	}
 
 	public void onResponse(Entry entry) {
@@ -23,16 +38,6 @@ public class ChatEntry {
 
 	public Entry getEntry() {
 		return entry;
-	}
-
-	public static void trigger(AsyncPlayerChatEvent e) {
-		final Player sender = e.getPlayer();
-		final ChatEntry chatEntry = entryMap.get(sender.getUniqueId());
-		if (chatEntry == null || chatEntry.getEntry() == null) return;
-		e.setCancelled(true);
-		chatEntry.getEntry().entry(e);
-		entryMap.remove(sender.getUniqueId());
-
 	}
 
 }
