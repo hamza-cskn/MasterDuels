b650e7f71bea3dea1e5054aa18a731c692383fab
Obliviated <hamzacoskun41@gmail.com>
2022-03-12 14:41:26 +0300

    feat: duel queue events added to api

---
+++ src/main/java/mc/obliviate/masterduels/queue/DuelQueueHandler.java
@@ -0,0 +1,19 @@
+package mc.obliviate.masterduels.queue;
+
+import mc.obliviate.masterduels.MasterDuels;
+import mc.obliviate.masterduels.queue.gui.DuelQueueListGUIUpdaterListener;
+import org.bukkit.Bukkit;
+
+public class DuelQueueHandler {
+
+	private final MasterDuels plugin;
+
+	public DuelQueueHandler(MasterDuels plugin) {
+		this.plugin = plugin;
+	}
+
+	public void initialize() {
+		Bukkit.getPluginManager().registerEvents(new DuelQueueListGUIUpdaterListener(), plugin);
+	}
+
+}
