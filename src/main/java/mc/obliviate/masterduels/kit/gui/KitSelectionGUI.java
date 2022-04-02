f8e6266c999a08ab29c6c673668b4f9878fc2d85
Obliviated <hamzacoskun41@gmail.com>
2022-04-02 05:18:43 +0300

    fix: kit selection gui auto-close code turned to pre-open tasks.

--- src/main/java/mc/obliviate/masterduels/kit/gui/KitSelectionGUI.java
+++ src/main/java/mc/obliviate/masterduels/kit/gui/KitSelectionGUI.java
@@ -8,22 +8,45 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 
+import java.util.Collection;
+import java.util.List;
+
 public class KitSelectionGUI extends GUI {
 
 	private final GameBuilder builder;
 	private final KitSelectResponse response;
+	private final List<Kit> allowedKits;
 
 	public KitSelectionGUI(Player player, GameBuilder builder, KitSelectResponse response) {
+		this(player, builder, response, null);
+	}
+
+	public KitSelectionGUI(Player player, GameBuilder builder, KitSelectResponse response, List<Kit> allowedKits) {
 		super(player, "kit-selection-gui", "Select a kit", 6);
 		this.builder = builder;
 		this.response = response;
+		this.allowedKits = allowedKits;
+	}
+
+	private Collection<Kit> getAllowedKits() {
+		if (allowedKits == null) return Kit.getKits().values();
+		return allowedKits;
+	}
+
+	@Override
+	public void open() {
+		if (getAllowedKits().isEmpty()) {
+			response.onSelected(null);
+			return;
+		}
+		super.open();
 	}
 
 	@Override
 	public void onOpen(InventoryOpenEvent event) {
 
 		int slot = 0;
-		for (final Kit kit : Kit.getKits().values()) {
+		for (final Kit kit : getAllowedKits()) {
 			final Icon icon = new Icon(kit.getIcon().clone()).onClick(e -> {
 				player.closeInventory();
 				builder.setKit(kit);
@@ -32,12 +55,6 @@ public class KitSelectionGUI extends GUI {
 
 			addItem(slot++, icon);
 		}
-		if (slot == 0) {
-			Bukkit.getScheduler().runTaskLater(getPlugin(), () -> player.closeInventory(), 1);
-			response.onSelected(null);
-		}
-
-
 	}
 
 	public interface KitSelectResponse {
