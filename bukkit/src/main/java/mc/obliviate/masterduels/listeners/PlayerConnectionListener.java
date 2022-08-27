00e57147824a0cce7b5ec230a9bd0d1294f41117
Obliviated <hamzacoskun41@gmail.com>
2022-08-27 10:40:46 +0300

    fix: fix player saving bug during server shutdown.

--- bukkit/src/main/java/mc/obliviate/masterduels/listeners/PlayerConnectionListener.java
+++ bukkit/src/main/java/mc/obliviate/masterduels/listeners/PlayerConnectionListener.java
@@ -18,19 +18,24 @@ public class PlayerConnectionListener implements Listener {
 		this.plugin = plugin;
 	}
 
-	@EventHandler
-	public void onDisconnect(PlayerQuitEvent event) {
-		final IUser user = UserHandler.getUser(event.getPlayer().getUniqueId());
-		if (user == null) return;
-		if (user instanceof Member) {
-			((Member) user).getTeam().getMatch().getMatchState().leave(((Member) user));
-		} else if (user.getMatchBuilder() != null) {
-			MatchCreator creator = MatchCreator.getCreator(event.getPlayer().getUniqueId());
-			if (creator == null || creator.getOwnerPlayer() != event.getPlayer().getUniqueId() || !MatchCreator.cleanKillCreator(event.getPlayer().getUniqueId())) {
-				user.getMatchBuilder().removePlayer(user);
-			}
-		}
-	}
+    @EventHandler
+    public void onDisconnect(PlayerQuitEvent event) {
+        final IUser user = UserHandler.getUser(event.getPlayer().getUniqueId());
+        if (user == null) return;
+
+        if (!MasterDuels.isInShutdownMode()) {
+            plugin.getSqlManager().saveUser(user);
+        }
+
+        if (user instanceof Member) {
+            ((Member) user).getTeam().getMatch().getMatchState().leave(((Member) user));
+        } else if (user.getMatchBuilder() != null) {
+            MatchCreator creator = MatchCreator.getCreator(event.getPlayer().getUniqueId());
+            if (creator == null || creator.getOwnerPlayer() != event.getPlayer().getUniqueId() || !MatchCreator.cleanKillCreator(event.getPlayer().getUniqueId())) {
+                user.getMatchBuilder().removePlayer(user);
+            }
+        }
+    }
 
 	@EventHandler
 	public void onConnect(PlayerJoinEvent event) {
