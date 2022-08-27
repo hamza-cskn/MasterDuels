3a05301bc451b523fcd2eec2c4a8802e2e653c07
Obliviated <hamzacoskun41@gmail.com>
2022-08-27 10:41:38 +0300

    fix: fix duel user deserializing issues

--- bukkit/src/main/java/mc/obliviate/masterduels/user/User.java
+++ bukkit/src/main/java/mc/obliviate/masterduels/user/User.java
@@ -22,20 +22,24 @@ public class User implements IUser {
 		this.statistic = statistic;
 	}
 
-	protected static User loadDuelUser(SQLManager sqlManager, Player player) {
-		if (sqlManager.getPlayerDataTable().exist(player.getUniqueId())) {
-			ResultSet rs = sqlManager.getPlayerDataTable().select(player.getUniqueId().toString());
-			DuelStatistic statistic = SQLManager.deserializeStatistic(rs, false);
-			try {
-				boolean receivesInvites = rs.getBoolean("receivesInvites");
-				return new User(player, receivesInvites, statistic);
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
-		} else {
-			return new User(player, true, DuelStatistic.createDefaultInstance(player.getUniqueId()));
-		}
-	}
+    protected static User loadDuelUser(SQLManager sqlManager, Player player) {
+        if (sqlManager.getPlayerDataTable().exist(player.getUniqueId())) {
+            ResultSet resultSet = sqlManager.getPlayerDataTable().select(player.getUniqueId().toString());
+            try {
+                UUID uuid = UUID.fromString(resultSet.getString("uuid"));
+                DuelStatistic statistic = sqlManager.getStatistic(uuid);
+                boolean receivesInvites = resultSet.getBoolean("receivesInvites");
+                boolean showScoreboard = resultSet.getBoolean("showScoreboard");
+                boolean showBossBar = resultSet.getBoolean("showBossBar");
+                return new User(Bukkit.getPlayer(uuid), receivesInvites, showScoreboard, showBossBar, statistic);
+            } catch (SQLException e) {
+                Logger.severe("User data of " + player.getName() + " could not loaded.");
+                throw new RuntimeException(e);
+            }
+        }
+        return new User(player, true, true, true, DuelStatistic.createDefaultInstance(player.getUniqueId()));
+
+    }
 
 	public boolean isInMatchBuilder() {
 		return matchBuilder != null;
