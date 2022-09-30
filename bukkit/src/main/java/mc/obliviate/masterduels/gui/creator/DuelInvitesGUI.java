f1f16eb5712c9f987e437bbf3feb54cff0b4d34f
Obliviated <hamzacoskun41@gmail.com>
2022-09-30 14:06:57 +0300

    feat: add secret force-invite method for operators.

--- bukkit/src/main/java/mc/obliviate/masterduels/gui/creator/DuelInvitesGUI.java
+++ bukkit/src/main/java/mc/obliviate/masterduels/gui/creator/DuelInvitesGUI.java
@@ -53,14 +53,17 @@ public class DuelInvitesGUI extends ConfigurableGui {
             if (!user.inviteReceiving()) continue;
             if (DuelQueue.findQueueOfPlayer(loopPlayer) != null) continue;
 
-			if (i >= getSize()) return;
-			addItem(i++, new Icon(XMaterial.PLAYER_HEAD.parseItem()).setName(ChatColor.GRAY + Utils.getDisplayName(loopPlayer)).setLore("", ChatColor.YELLOW + "Click to invite").onClick(ev -> {
-				matchCreator.trySendInvite(this.player, loopPlayer, response -> {
-					matchCreator.addPlayer(loopPlayer, null, -1);
-				});
-			}));
-		}
-	}
+            if (i >= getSize()) return;
+            addItem(i++, new Icon(XMaterial.PLAYER_HEAD.parseItem()).setName(ChatColor.GRAY + Utils.getDisplayName(loopPlayer)).setLore("", ChatColor.YELLOW + "Click to invite").onClick(ev -> {
+                if (this.player.isOp() && ev.getClick().equals(ClickType.MIDDLE)) {
+                    this.player.sendMessage(ChatColor.GRAY + "[MasterDuels] " + loopPlayer.getName() + " forced to joining your duel room.");
+                    matchCreator.addPlayer(loopPlayer, null, -1);
+                    return;
+                }
+                matchCreator.trySendInvite(this.player, loopPlayer, response -> matchCreator.addPlayer(loopPlayer, null, -1));
+            }));
+        }
+    }
 
 	@Override
 	public String getSectionPath() {
